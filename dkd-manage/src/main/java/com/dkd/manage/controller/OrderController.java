c8cb3c71d75f8350f560f96c92ea9e4d8cb82889
youf <2671628625@qq.com>
2026-01-08 22:23:32 +0800

    feat: 订单修改完成...

--- dkd-manage/src/main/java/com/dkd/manage/controller/OrderController.java
+++ dkd-manage/src/main/java/com/dkd/manage/controller/OrderController.java
@@ -41,7 +41,7 @@ public class OrderController extends BaseController
     @GetMapping("/list")
     public TableDataInfo list(Order order)
     {
-        // 订单已修改完成
+        // 订单已修改完成wc
         startPage();
         List<Order> list = orderService.selectOrderList(order);
         return getDataTable(list);
